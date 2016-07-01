90089a738cd077e4c855887d94ae1946f34629e8
Yanfei <wuxxx728@umn.edu>
2016-07-01 09:17:28 -0700

    Modified script.

--- ex2_logistic regression/costFunctionReg.m
+++ ex2_logistic regression/costFunctionReg.m
@@ -16,8 +16,10 @@ grad = zeros(size(theta));
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
-
-
+h = sigmoid(X*theta);
+J = 1/m * sum(-y.*log(h) - (1-y).*log(1-h)) + lambda/(2*m)*(sum(theta.^2)-theta(1)^2);
+grad = 1/m * X'*(h-y) + lambda/m * theta;
+grad(1) = grad(1) - lambda/m * theta(1);
 
 
 
