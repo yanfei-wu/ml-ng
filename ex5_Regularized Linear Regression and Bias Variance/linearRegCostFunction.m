045233aa2c8e2d4cfcd1c579690f169738550848
Yanfei <wuxxx728@umn.edu>
2016-08-04 22:13:24 -0700

    modified script

--- ex5_Regularized Linear Regression and Bias Variance/linearRegCostFunction.m
+++ ex5_Regularized Linear Regression and Bias Variance/linearRegCostFunction.m
@@ -18,8 +18,9 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
-
-
+J = 1/(2*m)*sum((X*theta-y).^2) + lambda/(2*m)*(sum((theta).^2) - theta(1).^2);
+grad = 1/m*(X'*(X*theta-y)) + lambda/m*theta;;
+grad(1) = grad(1) - lambda/m * theta(1);
 
 
 
